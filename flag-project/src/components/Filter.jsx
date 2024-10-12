1e418c2cc8d1ae045dce10eb46bdd931d7b4916a
Your Name <matilda.hagerman@hey.com>
2024-10-12 14:09:41 +0200

    Dropdown Filter label text

--- flag-project/src/components/Filter.jsx
+++ flag-project/src/components/Filter.jsx
@@ -3,18 +3,21 @@ import React, { useState } from 'react';
 
 function Filter({onFilterChange}) {
   const [selectedOption, setSelectedOption] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
 
   const handleChange = (event) => {
     const region = event.target.value;
     setSelectedOption(region);
-
     onFilterChange(region);
   };
 
   return (
-    <div>
-      <select value={selectedOption} onChange={handleChange} className='filter-dropdown'>
-        <option value="">Region</option>
+    <div className="filter-container">
+      <label className={`filter-label ${selectedOption || isFocused ? 'active' : ''}`}>
+        Region
+      </label>
+      <select value={selectedOption} onChange={handleChange} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} className='filter-dropdown'>
+        <option value=""> </option>
         <option value="All">All</option>
         <option value="Africa">Africa</option>
         <option value="Americas">Americas</option>
