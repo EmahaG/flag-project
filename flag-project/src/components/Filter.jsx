b179726beabdfb135a28731a4849a4f5a50b9419
Your Name <matilda.hagerman@hey.com>
2024-10-13 13:03:45 +0200

    Dropdown ref hide on click outside filter

--- flag-project/src/components/Filter.jsx
+++ flag-project/src/components/Filter.jsx
@@ -1,5 +1,5 @@
 import "./Filter.css";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import arrowDark from "../assets/arrow-down-black.svg";
 import arrowLight from "../assets/arrow-down-white.svg";
 
@@ -7,7 +7,31 @@ function Filter({ onFilterChange, darkMode }) {
   const [selectedOption, setSelectedOption] = useState("");
   const [isFocused, setIsFocused] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
   const options = ["All", "Africa", "Americas", "Asia", "Europe", "Oceania"];
+
+    // Close the dropdown if clicked outside
+    useEffect(() => {
+      const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+          setIsOpen(false); // Close the dropdown
+        }
+      };
+  
+      // Add event listener to the whole document
+      document.addEventListener("mousedown", handleClickOutside);
+  
+      // Cleanup the event listener
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [dropdownRef]);
+  
+    const handleChange = (event) => {
+      const region = event.target.value;
+      setSelectedOption(region);
+      onFilterChange(region);
+    };
 
   const handleOptionClick = (option) => {
     setSelectedOption(option);
@@ -24,7 +48,7 @@ function Filter({ onFilterChange, darkMode }) {
       >
         Region
       </label>
-      <div className="custom-dropdown" onClick={() => setIsOpen(!isOpen)}>
+      <div className="custom-dropdown" onClick={() => setIsOpen(!isOpen)} ref={dropdownRef}>
         <div className="selected-option">
           {selectedOption || " "}
         </div>
